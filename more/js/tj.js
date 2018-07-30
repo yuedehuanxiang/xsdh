052e98786330597df653da011ed8dbb181a33b88
UXH <24637370+uxh@users.noreply.github.com>
2018-07-30 18:25:12 +0800

    Version 1.0

---
+++ more/js/tj.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?03487b41c1b45d1fdac3082e7206f169";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
